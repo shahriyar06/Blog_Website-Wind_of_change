9804a2add66b5679863902112a82305b75e2db9f
Shahriyar <chyshahriyar@gmail.com>
2023-12-14 02:35:13 +0600

    Settings and switch

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import Topbar from "./Items/topbar/Topbar";
 import Home from "./pages/home/Home";
-
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+import Settings from "./pages/settings/Settings";
 import Single from "./pages/single/Single";
-
+import Write from "./pages/write/Write";
 import React from "react";
 import {
   BrowserRouter as Router,

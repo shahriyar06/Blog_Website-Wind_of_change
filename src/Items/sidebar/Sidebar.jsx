6fa14ead6e9c2e2218d757e29182629d13372683
Shahriyar <chyshahriyar@gmail.com>
2023-12-13 19:20:45 +0600

    Sidebar

---
+++ src/Items/sidebar/Sidebar.jsx
@@ -0,0 +1,67 @@
+import { Link } from "react-router-dom";
+import "./Sidebar.css";
+
+export default function Sidebar() {
+  return (
+    <div className="sidebar">
+      <div className="sidebarItem">
+        <span className="sidebarTitle">ABOUT ME</span>
+        <img
+          src="https://images.pexels.com/photos/160107/pexels-photo-160107.jpeg"
+          alt=""
+        />
+        <p>
+        My name is At Tatfif Hossain. I did my graduation from Kalyani University, jiaganj, Murshidabad.
+        Currently, I'm pursuing an industrial training institute, Berhampore.
+        I belong to a nuclear family. My strengths are I'm an honest, self-motivated, a quick learner and hard-working.
+        My short-term goal is to get a government service in a reputed sector and my long-term goal is to achieve a good position where I can build my career.
+        My hobbies are reading, writing and eating all types of delicious food. I'm a disciplined person and believe in learning from my past mistakes and experiences.
+        </p>
+      </div>
+      <div className="sidebarItem">
+        <span className="sidebarTitle">CATEGORIES</span>
+        <ul className="sidebarList">
+          <li className="sidebarListItem">
+            <Link className="link" to="/posts?cat=Life">
+              Life
+            </Link>
+          </li>
+          <li className="sidebarListItem">
+            <Link className="link" to="/posts?cat=Music">
+              Music
+            </Link>
+          </li>
+          <li className="sidebarListItem">
+            <Link className="link" to="/posts?cat=Sport">
+              Sport
+            </Link>
+          </li>
+          <li className="sidebarListItem">
+            <Link className="link" to="/posts?cat=Style">
+              Style
+            </Link>
+          </li>
+          <li className="sidebarListItem">
+            <Link className="link" to="/posts?cat=Tech">
+              Tech
+            </Link>
+          </li>
+          <li className="sidebarListItem">
+            <Link className="link" to="/posts?cat=Cinema">
+              Cinema
+            </Link>
+          </li>
+        </ul>
+      </div>
+      <div className="sidebarItem">
+        <span className="sidebarTitle">FOLLOW US</span>
+        <div className="sidebarSocial">
+          <i className="sidebarIcon fab fa-facebook-square"></i>
+          <i className="sidebarIcon fab fa-instagram-square"></i>
+          <i className="sidebarIcon fab fa-pinterest-square"></i>
+          <i className="sidebarIcon fab fa-twitter-square"></i>
+        </div>
+      </div>
+    </div>
+  );
+}

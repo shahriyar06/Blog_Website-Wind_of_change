5cfee042ba50e1431cb2a998a5132eb118d20eb4
Tanveer <titanveer2000@gmail.com>
2023-12-13 19:51:10 +0600

    Post

---
+++ src/Items/post/Post.jsx
@@ -0,0 +1,38 @@
+import { Link } from "react-router-dom";
+import "./Post.css";
+
+export default function Post({img}) {
+  return (
+    <div className="post">
+      <img
+        className="postImg"
+        src={img}
+        alt=""
+      />
+      <div className="postInfo">
+        <div className="postCats">
+          <span className="postCat">
+            <Link className="link" to="/posts?cat=Music">
+              Music
+            </Link>
+          </span>
+          <span className="postCat">
+            <Link className="link" to="/posts?cat=Music">
+              Life
+            </Link>
+          </span>
+        </div>
+        <span className="postTitle">
+          <Link to="/post/abc" className="link">
+            Lorem ipsum dolor sit amet
+          </Link>
+        </span>
+        <hr />
+        <span className="postDate">1 hour ago</span>
+      </div>
+      <p className="postDesc">
+      A blog (a truncation of "weblog") is an informational website consisting of discrete, often informal diary-style text entries (posts). Posts are typically displayed in reverse chronological order so that the most recent post appears first, at the top of the web page. Until 2009, blogs were often the work of a single individual, occasionally of a small group, and often covered a single subject or topic. In the 2010s, "multi-author blogs" (MABs) emerged, featuring the writing of multiple authors and sometimes professionally edited. MABs from newspapers, other media outlets, universities, think tanks, advocacy groups, and similar institutions account for an increasing quantity of blog traffic. The rise of Twitter and other "microblogging" systems helps integrate MABs and single-author blogs into the news media. Blog can also be used as a verb, meaning to maintain or add content to a blog.
+      </p>
+    </div>
+  );
+}
